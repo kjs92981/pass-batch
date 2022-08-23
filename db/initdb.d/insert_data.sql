c75cd162a99a2513e9eb298b8f3a67605784dc3b
kjs92980 <kjs9298@naver.com>
2022-08-23 13:44:27 +0900

    ch03-02 class

---
+++ db/initdb.d/insert_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO pass_local.pass (gym_id, pass_id, user_id, status, num, started_at, ended_at, expired_at, created_at, deleted) VALUES
+('GYM', '한글이외다', '10000', 'ACTIVE', 100, '2022-08-15 00:00:00', '2022-08-16 00:00:00', NULL, '2022-08-15 00:00:00', 0);
